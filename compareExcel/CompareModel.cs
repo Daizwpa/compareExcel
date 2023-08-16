374d00d7b8be668041d2dd0724568d41ed4b2e77
User <Daizwpa>
2023-08-16 21:57:13 +0100

    Add project files.

---
+++ compareExcel/CompareModel.cs
@@ -0,0 +1,139 @@
+﻿using IronXL;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compareExcel
+{
+    public class CompareModel
+    {
+
+        string[] columns1;
+        string[] columns2;
+        string path1;
+        string path2;
+
+
+        public CompareModel(string columns1, string columns2, string path1, string path2)
+        {
+            this.path1 = path1;
+            this.path2 = path2;
+            this.columns1 = columns1.Split(',').Select(c => c.Trim()).ToArray();
+            this.columns2 = columns2.Split(",").Select(c => c.Trim()).ToArray();
+        }
+
+        public DataTable Check()
+        {
+
+            DataTable content = CreateTAble(columns1);
+            
+            WorkSheet file1 = ReadExcel(path1);
+            WorkSheet file2 = ReadExcel(path2);
+
+            for (int row1 = 1; row1 <= file1.Rows.Length; row1++)
+            {
+                string[] values = getValues(file1, row1, columns1);
+
+                bool found = isExists(file2, values);
+
+                if (!found)
+                    content.Rows.Add(values);
+
+            }
+            return content;
+        
+        }
+
+        private bool isExists(WorkSheet source, string[] values)
+        {       bool found = false;
+                for (int i = 1;  i <= source.RowCount; i++)
+                {
+                    bool[] tablebool = new bool[columns2.Length];
+                    for (int col = 0; col < columns2.Length; col++)
+                    {
+                        tablebool[col] = false;
+                        if (source[columns2[col] + i].Value.ToString() == values[col])
+                        {
+                            tablebool[col] = true;
+
+                            break;
+                        }
+
+                    }
+                    if(tablebool.Any(s => s == true))
+                    {
+                        found = true;
+                        break;
+                    }
+                    
+                }
+
+            return found;
+
+        }
+
+
+        private DataTable CreateTAble(string[] cols)
+        {
+
+            DataTable table = new DataTable("compare");
+
+            for (int col = 0; col < columns1.Length; col++)
+            {
+                table.Columns.Add(cols[col], typeof(string));
+
+            }
+            return table;
+        }
+
+
+        private string[] getValues(WorkSheet  table,int row1, string[] cols)
+        {
+            string[] value = new string[cols.Length];
+            
+
+            try
+            {
+                if (table == null) throw new InvalidOperationException();
+                if (cols == null) throw new ArgumentNullException();
+
+
+                for (int j = 0; j < value.Length; j++)
+                {
+                    value[j] = table[cols[j] + row1].Value.ToString() ?? string.Empty;
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            return value;
+
+
+        }
+
+        /// <summary>
+        /// this method will read the excel file and copy its data into a datatable
+        /// </summary>
+        /// <param name="fileName">name of the file</param>
+        /// <returns>DataTable</returns>
+        private WorkSheet ReadExcel(string fileName)
+        {
+            WorkBook workbook = WorkBook.Load(fileName);
+            //// Work with a single WorkSheet.
+            ////you can pass static sheet name like Sheet1 to get that sheet
+            ////WorkSheet sheet = workbook.GetWorkSheet("Sheet1");
+            //You can also use workbook.DefaultWorkSheet to get default in case you want to get first sheet only
+            WorkSheet sheet = workbook.DefaultWorkSheet;
+            //Convert the worksheet to System.Data.DataTable
+            //Boolean parameter sets the first row as column names of your table.
+            return sheet;
+        }
+    }
+}
---
+++ compareExcel/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace compareExcel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void addexecl1(object sender, RoutedEventArgs e)
+        {
+            string path = path_fileExecl(1);
+            show1.Text = path;
+        }
+
+        private void addexecl2(object sender, RoutedEventArgs e)
+        {
+            string path = path_fileExecl(2);
+            show2.Text = path;
+        }
+
+        private void CompareEvent(object sender, RoutedEventArgs e)
+        {
+            progessRing.Visibility = Visibility.Visible;
+
+            try
+            {
+                var checker = new CompareModel(columnsBox1.Text, columnsBox2.Text, show1.Text, show2.Text);
+                DataTable dt = checker.Check();
+                dg1.ItemsSource = dt.AsDataView();
+
+                checker = new CompareModel(columnsBox2.Text, columnsBox1.Text, show2.Text, show1.Text);
+                DataTable dt2 = checker.Check();
+                dg2.ItemsSource = dt2.AsDataView();
+
+            }
+            catch
+            {
+
+            }finally { progessRing.Visibility = Visibility.Hidden; }
+
+        }
+
+        string path_fileExecl(int numFile)
+        {
+            string path = string.Empty;
+
+            var openFileDialog1 = new OpenFileDialog()
+            {
+                Filter = "Excel Files|*.xls;*.xlsx;*.xlsm",
+                Title = "select excel  " + numFile,
+            };
+            
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                path = openFileDialog1.FileName;
+            }
+            return path;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
